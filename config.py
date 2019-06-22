96a1be7b39c07cad9235880d98bc7c1231a6d453
Antoine Finot <antoine.finot@mail.utoronto.ca>
2019-06-22 11:15:48 +0100

    initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+pyrebase = {
+
+}
+
+secret_key = b'3dd5966f147d52bf076f12492c2807f1381798ee96c92395'
---
+++ main.py
@@ -0,0 +1,21 @@
+@app.route('/')
+def index():
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+@app.route('/logout', methods=['GET'])
+def logout():
+
+@app.route('/promises', methods=['GET'])
+def promises():
+
+@app.route('/create_promise', methods=['GET', 'POST'])
+def create_promise():
+
+@app.route('/invitation', methods=['GET'])
+def invitation():
+
