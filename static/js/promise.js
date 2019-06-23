180e52bccd467dd346829744aa1fd5726357f29d
Shawn Lu <shawnlu96@gmail.com>
2019-06-23 01:44:32 +0100

    small changes over promise

--- static/js/promise.js
+++ static/js/promise.js
@@ -1,1 +1,12 @@
 M.AutoInit();
+
+function createBar() {
+    var num = parseInt(Math.random()*101,10);
+    var barwrapper = document.createElement('div');
+    barwrapper.className='progress';
+    var bar = document.createElement('div');
+    bar.className='determinate';
+    bar.style="width: "+num+"%";
+    barwrapper.appendChild(bar);
+    document.getElementById("qq").appendChild(barwrapper);
+}
