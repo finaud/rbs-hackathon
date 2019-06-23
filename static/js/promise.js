78922dba664f0b2a26ff1c1f954bea314ad9c9d7
Shawn Lu <shawnlu96@gmail.com>
2019-06-23 02:45:53 +0100

    compatibility between icons and bars

--- static/js/promise.js
+++ static/js/promise.js
@@ -2,11 +2,22 @@ M.AutoInit();
 
 function createBar() {
     var num = parseInt(Math.random()*101,10);
+    if(num%4===0){num=100;}
     var barwrapper = document.createElement('div');
-    barwrapper.className='progress';
+    barwrapper.className='progress col m10 bar';
     var bar = document.createElement('div');
     bar.className='determinate';
     bar.style="width: "+num+"%";
     barwrapper.appendChild(bar);
-    document.getElementById("qq").appendChild(barwrapper);
+    var icondiv = document.createElement("div");
+    icondiv.className="col m2";
+    icondiv.style="text-align: center";
+    var icon = document.createElement('i');
+    icon.className="material-icons";
+    if(num===100){icon.innerText="check";}
+    else {icon.innerText="access_time";}
+    icondiv.appendChild(icon);
+    document.getElementById('qq').appendChild(icondiv);
+        document.getElementById("qq").appendChild(barwrapper);
+
 }
